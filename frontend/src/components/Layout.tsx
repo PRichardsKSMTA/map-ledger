f7908b72346d42bd5469f65180f48a033b49a3e5
Pete Richards <pete.richards@ksmcpa.com>
2025-10-27 17:48:13 -0600

    Update layout to use full viewport width

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
@@ -10,7 +9,7 @@ export default function Layout() {
       <div className="flex-1 flex flex-col overflow-hidden">
         <Navbar />
         <main className="flex-1 overflow-y-auto p-6">
-          <div className="max-w-7xl mx-auto">
+          <div className="max-w-8xl mx-auto">
             <Outlet />
           </div>
         </main>
