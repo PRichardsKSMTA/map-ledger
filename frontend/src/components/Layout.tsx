9647455cf500dcb72364b2087b9a39a48ff74e28
Pete Richards <pete.richards@ksmcpa.com>
2025-10-27 17:58:10 -0600

    Update layout for all pages

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -6,10 +6,12 @@ export default function Layout() {
   return (
     <div className="flex h-screen bg-gray-50">
       <Sidebar />
-      <div className="flex flex-1 flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col overflow-hidden">
         <Navbar />
-        <main className="flex-1 overflow-y-auto">
-          <Outlet />
+        <main className="flex-1 overflow-y-auto p-6">
+          <div className="max-w-8xl mx-auto">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
