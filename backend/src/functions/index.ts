1de8e6d86082f1c802338b16f293e95c309a34c7
Pete Richards <pete.richards@ksmcpa.com>
2025-12-03 16:28:38 -0700

    Add client file entity CRUD functions and repository

--- backend/src/functions/index.ts
+++ backend/src/functions/index.ts
@@ -13,6 +13,7 @@ import './datapointConfigs/create';
 import './datapointConfigs/list';
 import './datapointConfigs/update';
 import './clientFiles';
+import './clientFileEntities';
 import './clientEntities';
 import './clientHeaderMappings';
 import './fileRecords';
